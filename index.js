3c111cb6bb209d85f5793b9640f057d3876b65f5
Dice-K39 <daisukehashimoto39@gmail.com>
2020-11-08 03:53:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+function map(sourceArray, f)
+{
+    let array = [];
+
+    sourceArray.forEach(element =>
+    {
+        array.push(f(element));
+    });
+
+    return array;
+}
+
+function reduce(sourceArray, f, startingPoint)
+{
+    let answer = !!startingPoint ? startingPoint : sourceArray[0];
+    let i = !!startingPoint ? 0 : 1;
+
+    for (; i < sourceArray.length; i++)
+    {
+        answer = f(sourceArray[i], answer);
+    }
+
+    return answer;
+}
