4f86f141e264eac3d4e48d5f961bc98840b13234
baker615512 <baker615512@gmail.com>
2020-11-14 20:48:27 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your functions here
+function map(sourceArray, func){
+  let newArr = []
+  sourceArray.forEach((i)=>{
+    newArr.push(func(i))
+  })
+  return newArr
+}
+
+function reduce(sourceArray, func, startingPoint){
+  let total = sourceArray[0]
+  if (startingPoint){
+    total = startingPoint
+    for (let i = 0; i < sourceArray.length; i++){
+      total = func(total, sourceArray[i]) 
+    }
+  } else {
+    for (let i = 1; i < sourceArray.length; i++){
+      total = func(total, sourceArray[i]) 
+    }
+  }
+  return total
+}
