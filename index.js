eaa27084dfb8c425c7abef7a22b64e6a36e38506
chica25 <patricia@patriciaco.com>
2020-11-23 00:32:56 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your functions here
+
+function map(array, callBackFunc) {
+    return array.map(a => callBackFunc(a));
+}
+
+
+// function map(array) {
+//     return array.map(a => a);
+// }
+
+// function map(array) {
+//     return array.map(a => a * 2);
+// }
+
+// function map(array) {
+//     return array.map( a => a ** 2);
+// }
+
+
+function reduce(sourceArray, a, startingPoint){
+    let e = startingPoint ? startingPoint : sourceArray[0]
+    let i = startingPoint ? 0 : 1
+  
+    for (; i < sourceArray.length; i++) {
+      e = a(sourceArray[i], e);
+    }
+  
+    return e ;
+  }
